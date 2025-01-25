d2e40b67642ecc7760653bf06d00f33a34e6693f
javab <javab@Babulu>
2025-01-26 01:10:37 +0530

    ok

--- 01-Coding_Practice/src/Example/Test.java
+++ 01-Coding_Practice/src/Example/Test.java
@@ -6,8 +6,9 @@ public class Test {
 	public static void main(String[] args) {
 		int []a=new int[135];
 		String s="ss";
+		System.out.println("ok");
 		for(int i=0;i<s.length();i++) {
-		
+		 
 		a[s.charAt(i)]++;
 		}
 		System.out.println(Arrays.toString(a));
